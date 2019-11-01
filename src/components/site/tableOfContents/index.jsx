606aff46c548ae06b95ef1822f6bca8453f5826a
George Gomez <ggomez@SpareMacbookPro.local>
2019-11-01 14:52:57 -0700

    try to load drawer. throws Functions are not valid as a React child error....can't figure out yet

--- src/components/site/tableOfContents/index.jsx
+++ src/components/site/tableOfContents/index.jsx
@@ -2,28 +2,38 @@ import React from 'react';
 import { Button, Drawer, Toolbar } from 'react-md';
 import PropTypes from 'prop-types';
 import styles from './table-of-contents.module.scss';
+import NavItemLink from './NavItemLink';
+import ArrowLeft from '../icons/ArrowLeft';
 
-export class TableOfContents extends React.PureComponent {
+const TO_PREFIX = '/';
+const tocListItems = [
+  {
+    label: 'Inbox',
+    to: TO_PREFIX,
+    icon: 'inbox',
+  },
+  {
+    label: 'Inbox',
+    to: TO_PREFIX + 'test',
+    icon: 'inbox',
+  },
+];
+class TableOfContents extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    const { openSidebar } = props;
-
     this.state = {
-      visible: openSidebar,
+      visible: false,
     };
   }
 
-  openDrawerLeft = () => {
-    this.setState({
-      visible: true,
-    });
-  };
+  componentDidUpdate() {
+    const { openSidebar } = this.props;
+    this.toggleDrawer(openSidebar);
+  }
 
-  closeDrawer = () => {
-    this.setState({
-      visible: false,
-    });
+  toggleDrawer = bool => {
+    this.setState({ visible: bool });
   };
 
   handleVisibility = visible => {
@@ -31,25 +41,19 @@ export class TableOfContents extends React.PureComponent {
   };
 
   render() {
+    const { TEMPORARY } = Drawer.DrawerTypes;
     const { visible } = this.state;
 
     return (
       <Drawer
         id={styles.table_of_contents}
+        type={TEMPORARY}
         className={styles.tableOfContents}
         visible={visible}
-        position="left"
         onVisibilityChange={this.handleVisibility}
-        // navItems={tocListItems}
-        header={
-          <Toolbar
-            nav={
-              <Button icon onClick={this.closeDrawer}>
-                arrow_back
-              </Button>
-            }
-          />
-        }
+        navItems={tocListItems.map(props => (
+          <NavItemLink {...props} key={props.to} />
+        ))}
       />
     );
   }
