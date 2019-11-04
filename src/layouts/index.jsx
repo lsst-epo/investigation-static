edb88861f32121695f85317075e3381a906f2521
George Gomez <ggomez@SpareMacbookPro.local>
2019-11-04 10:42:51 -0700

    setting up to try the navigation drawer component

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavigationDrawer } from 'react-md';
 import GlobalStore from '../state/GlobalStore';
 // import '../assets/stylesheets/styles.scss';
 import styles from './layout.module.scss';
 import Header from '../components/site/header';
-import TableOfContents from '../components/site/tableOfContents';
+// import TableOfContents from '../components/site/tableOfContents';
 // import Footer from '../components/site/footer';
-
+const routes = [
+  {
+    to: '',
+    exact: 'true',
+    icon: 'home',
+  },
+  {
+    to: 'testing',
+    icon: 'list',
+  },
+];
 class Layout extends React.Component {
   constructor() {
     super();
@@ -21,23 +32,30 @@ class Layout extends React.Component {
     };
   }
 
+  navItems = routes.map(route => {
+    return route;
+  });
+
   toggleSidebar = () => {
     const { openSidebar } = this.state;
     this.setState({ openSidebar: !openSidebar });
   };
 
   render() {
-    const { openSidebar } = this.state;
+    // const { openSidebar } = this.state;
     const { children } = this.props;
     return (
-      <div style={{ minHeight: '100vh' }}>
+      <NavigationDrawer
+        drawerId="main-table-of-contents"
+        drawerTitle="Table of Contents"
+        navItems={this.navItems}
+      >
         <Header siteTitle="Investigation" toggleSidebar={this.toggleSidebar} />
-        <TableOfContents openSidebar={openSidebar} />
         <div>
           <main className={styles.container}>{children}</main>
         </div>
         {/* <Footer /> */}
-      </>
+      </NavigationDrawer>
     );
   }
 }
