8c3616c783dc6f2bcf7335a9c579e7596e601f29
George Gomez <ggomez@SpareMacbookPro.local>
2019-11-05 16:50:08 -0700

    drawer now works and routes are correctly loaded

--- src/components/site/tableOfContents/index.jsx
+++ src/components/site/tableOfContents/index.jsx
@@ -1,49 +1,73 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import reactn from 'reactn';
+import { Link, graphql, StaticQuery } from 'gatsby';
 import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
-import { Button, Drawer, Toolbar } from 'react-md';
-import ArrowLeft from '../icons/ArrowLeft';
+import { Drawer } from 'react-md';
 import styles from './table-of-contents.module.scss';
 
+// import Footer from '../components/site/footer';
+const TO_PREFIX = '/';
+const routes = [
+  {
+    label: 'home',
+    to: `${TO_PREFIX}`,
+    icon: 'home',
+    exact: 'true',
+    primaryText: 'Home',
+    active: false,
+  },
+  {
+    label: 'style-guide',
+    to: `${TO_PREFIX}StyleGuide`,
+    icon: 'style',
+    primaryText: 'Style Guide',
+    active: false,
+  },
+  { divider: true },
+  {
+    primaryText: 'Table of Contents',
+    subheader: true,
+  },
+];
+
+@reactn
 class TableOfContents extends React.PureComponent {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
+  //
+  // }
 
-    this.state = {
-      visible: false,
-    };
-
-    this.navLinks = props.tocLinks.map(link => {
+  componentDidMount() {
+    const { navLinks } = this.props;
+    const links = routes.concat(navLinks);
+    this.navLinks = links.map(link => {
       if (link.divider || link.subheader) return link;
 
       if (link.to) {
         link.component = Link;
       }
+
+      if (link.slug) {
+        link.component = Link;
+        link.label = link.title;
+        link.to = `${TO_PREFIX}${link.slug}`;
+        link.primaryText = link.title;
+        link.active = false;
+      }
       return link;
     });
-  }
-
-  componentDidUpdate() {
-    const { openSidebar: visible } = this.props;
-    this.handleVisibility(visible);
   }
 
   handleVisibility = visible => {
     const { toggleSidebar } = this.props;
     toggleSidebar(visible);
-    this.setState({ visible });
   };
 
   render() {
     const { TEMPORARY } = Drawer.DrawerTypes;
-    const { visible } = this.state;
-    const { toggleSidebar } = this.props;
+    const { visible } = this.props;
 
-    const closeBtn = (
-      <Button icon onClick={toggleSidebar}>
-        <ArrowLeft />
-      </Button>
-    );
     return (
       <Drawer
         type={TEMPORARY}
@@ -52,22 +76,34 @@ class TableOfContents extends React.PureComponent {
         onVisibilityChange={this.handleVisibility}
         navItems={this.navLinks}
         overlay
-        header={
-          <Toolbar
-            colored
-            actions={closeBtn}
-            className="md-divider-border md-divider-border--bottom"
-          />
-        }
       />
     );
   }
 }
 
 TableOfContents.propTypes = {
-  openSidebar: PropTypes.bool.isRequired,
-  tocLinks: PropTypes.array.isRequired,
+  visible: PropTypes.bool,
   toggleSidebar: PropTypes.func.isRequired,
+  navLinks: PropTypes.object,
 };
 
-export default TableOfContents;
+// export default TableOfContents;
+
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query MyQuery {
+        allPagesJson {
+          nodes {
+            title
+            slug
+            id
+          }
+        }
+      }
+    `}
+    render={data => (
+      <TableOfContents {...props} navLinks={data.allPagesJson.nodes} />
+    )}
+  />
+);
