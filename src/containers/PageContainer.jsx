31de851e9dab55c9d8912b5b689bedd34a32a7d5
George Gomez <ggomez@SpareMacbookPro.local>
2019-11-05 09:34:50 -0700

    semi-working drawer

--- src/containers/PageContainer.jsx
+++ src/containers/PageContainer.jsx
@@ -9,7 +9,6 @@ import isObject from 'lodash/isObject';
 import Page from '../components/page';
 import TwoCol from '../components/page/TwoCol.jsx';
 import PageNav from '../components/pageNav';
-
 @reactn
 class PageContainer extends React.PureComponent {
   constructor(props) {
