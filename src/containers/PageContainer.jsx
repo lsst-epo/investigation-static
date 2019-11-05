4dfa91c6a21ba6d06a5f71489e44b11076815c3b
Blake Mason <bmason@lsst.org>
2019-11-04 17:52:34 -0700

    [F] Integrate QA flow with new pages & questions
    Pages and questions have a new data structure, so many minor updates
    were needed to integrate previous work with new framework;
    Refactored Questions and Answers HOCs into PageContainer;
    Fixed typos in GlobalStore that were causing global state to update
    in unexpected ways (or not at all, depending on the reducer);

--- src/containers/PageContainer.jsx
+++ src/containers/PageContainer.jsx
@@ -1,8 +1,11 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import reactn from 'reactn';
-import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import isEmpty from 'lodash/isEmpty';
+import find from 'lodash/find';
+import includes from 'lodash/includes';
+import isObject from 'lodash/isObject';
 import Page from '../components/page';
 import TwoCol from '../components/page/TwoCol.jsx';
 import PageNav from '../components/pageNav';
@@ -17,17 +20,130 @@ class PageContainer extends React.PureComponent {
       'two-col': TwoCol,
       'single-col': Page,
     };
+
+    this.state = {
+      questions: null,
+      activeId: null,
+      activeQuestion: null,
+    };
   }
 
   componentDidMount() {
     const { data } = this.props;
-    const page = data.allPagesJson.nodes[0];
+    const { id, questionsByPage: questions } = data.allPagesJson.nodes[0];
+    const activeQuestion = this.getActiveQ(questions);
 
-    this.dispatch.updatePageId(page.id);
+    this.setState(prevState => ({
+      ...prevState,
+      questions,
+      activeId: activeQuestion ? activeQuestion.id : null,
+      activeQuestion,
+    }));
+
+    this.dispatch.updatePageId(id);
   }
+
+  // Helper methods related to determining "active QA"
+  qWithEmptyA(qArray, answers) {
+    return find(qArray, q => {
+      return isEmpty(answers[q.id]);
+    });
+  }
+
+  getActiveQ = questions => {
+    const { answers } = this.global;
+    const activeQ = find(questions, question => {
+      return !!this.qWithEmptyA(question.question, answers);
+    });
+
+    if (!activeQ) return null;
+
+    if (activeQ.question.length > 1) {
+      const nestedActiveQ = this.qWithEmptyA(activeQ.question, answers);
+
+      return nestedActiveQ || null;
+    }
+
+    return activeQ ? activeQ.question[0] : null;
+  };
+
+  // Methods related to setting & updating "active QA"
+
+  // Callback method passed down to child components
+  setActiveQuestion = activeQ => {
+    this.setState(prevState => ({
+      ...prevState,
+      activeQ,
+      activeId: activeQ ? activeQ.id : null,
+    }));
+  };
+
+  // Callback method passed down to child components
+  advanceActiveQuestion = () => {
+    const { questions } = this.state;
+    const activeQ = this.getActiveQ(questions);
+    this.setActiveQuestion(activeQ);
+  };
+
+  // Helper methods for identifying question being answered
+  qInArrayById(qs, id) {
+    const q = find(qs, nestedQ => {
+      return nestedQ.id === id;
+    });
+
+    return q;
+  }
+
+  qById(questions, id) {
+    const identifiedQ = find(questions, q => {
+      return !!this.qInArrayById(q.question, id);
+    });
+
+    if (identifiedQ.question.length > 1) {
+      return this.qInArrayById(identifiedQ.question, id);
+    }
+
+    return identifiedQ.question[0];
+  }
+
+  // Methods related to updating answers
+
+  updateAnswer(id, data) {
+    const { questions } = this.state;
+    const answeredQuestion = this.qById(questions, id);
+    const { answerAccessor } = answeredQuestion;
+    let content = data;
+
+    if (answerAccessor === 'text') {
+      content = data || '';
+      content = isObject(content) ? '' : content;
+    } else if (
+      answerAccessor === 'compound-select' ||
+      answerAccessor === 'select'
+    ) {
+      content = data;
+      content = isObject(content) ? 'DEFAULT' : content;
+    } else if (answerAccessor === 'count') {
+      content = data.length;
+    } else if (!includes(answerAccessor, 'range')) {
+      content = data[0] ? data[0][answerAccessor] : 'None Selected';
+    }
+
+    this.dispatch.updateAnswer(id, content, data);
+  }
+
+  // Callback method passed down to child components
+  answerHandler = (id, data, eventType) => {
+    if ((id && data) || eventType) {
+      this.updateAnswer(id, data);
+    } else {
+      this.dispatch.clearAnswer(id);
+    }
+  };
 
   render() {
     const { data } = this.props;
+    const { questions, activeId } = this.state;
     const {
       id,
       title,
@@ -35,7 +151,6 @@ class PageContainer extends React.PureComponent {
       previous,
       next,
       content,
-      questionsByPage,
     } = data.allPagesJson.nodes[0];
     const { answers } = this.global;
     const Tag = this.layouts[layout || 'single-col'];
@@ -48,8 +163,12 @@ class PageContainer extends React.PureComponent {
           previous={previous}
           next={next}
           content={content}
-          questions={questionsByPage}
+          questions={questions}
           answers={answers}
+          activeId={activeId}
+          answerHandler={this.answerHandler}
+          advanceActive={this.advanceActiveQuestion}
+          setActive={this.setActiveQuestion}
         />
         <PageNav previous={previous} next={next} />
       </div>
