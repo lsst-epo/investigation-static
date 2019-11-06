d27b8e708a290e319a7dcdb9b05accccbedec88e
georgegomez144 <george144k.dev@gmail.com>
2019-11-06 16:40:17 -0700

    move link const into class

--- src/components/site/header/index.jsx
+++ src/components/site/header/index.jsx
@@ -2,6 +2,7 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Button, Toolbar } from 'react-md';
+// import { Button } from '../button/index';
 import logo from '../../../images/lsst-logo.svg';
 import styles from './header.module.scss';
 import Close from '../icons/Close';
