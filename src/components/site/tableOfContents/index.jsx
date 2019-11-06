b95442f23a508cd0039dcc410d324e3cdc993514
georgegomez144 <george144k.dev@gmail.com>
2019-11-06 16:40:17 -0700

    move link const into class

--- src/components/site/tableOfContents/index.jsx
+++ src/components/site/tableOfContents/index.jsx
@@ -7,40 +7,39 @@ import { Drawer } from 'react-md';
 import styles from './table-of-contents.module.scss';
 
 // import Footer from '../components/site/footer';
-const TO_PREFIX = '/';
-const routes = [
-  {
-    label: 'home',
-    to: `${TO_PREFIX}`,
-    icon: 'home',
-    exact: 'true',
-    primaryText: 'Home',
-    active: false,
-  },
-  {
-    label: 'style-guide',
-    to: `${TO_PREFIX}StyleGuide`,
-    icon: 'style',
-    primaryText: 'Style Guide',
-    active: false,
-  },
-  { divider: true },
-  {
-    primaryText: 'Table of Contents',
-    subheader: true,
-  },
-];
 
 @reactn
 class TableOfContents extends React.PureComponent {
-  // constructor(props) {
-  //   super(props);
-  //
-  // }
+  constructor(props) {
+    super(props);
+    this.TO_PREFIX = '/';
+    this.routes = [
+      {
+        label: 'home',
+        to: `${this.TO_PREFIX}`,
+        icon: 'home',
+        exact: 'true',
+        primaryText: 'Home',
+        active: false,
+      },
+      {
+        label: 'style-guide',
+        to: `${this.TO_PREFIX}StyleGuide`,
+        icon: 'style',
+        primaryText: 'Style Guide',
+        active: false,
+      },
+      { divider: true },
+      {
+        primaryText: 'Table of Contents',
+        subheader: true,
+      },
+    ];
+  }
 
   componentDidMount() {
     const { navLinks } = this.props;
-    const links = routes.concat(navLinks);
+    const links = this.routes.concat(navLinks);
     this.navLinks = links.map(link => {
       if (link.divider || link.subheader) return link;
 
@@ -51,7 +50,7 @@ class TableOfContents extends React.PureComponent {
       if (link.slug) {
         link.component = Link;
         link.label = link.title;
-        link.to = `${TO_PREFIX}${link.slug}`;
+        link.to = `${this.TO_PREFIX}${link.slug}`;
         link.primaryText = link.title;
         link.active = false;
       }
