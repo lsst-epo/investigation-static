182bd09e023ed9fc5ebc7e2c37fe5fea26745404
Blake Mason <bmason@lsst.org>
2020-01-14 15:37:44 -0800

    [C] Set up basic 3D Hubble Plot page with data
    This page provides the data needed to develop the 3D Hubble Plot
    feature;  This commit only provides an empty component, it does not
    include any work developing the Plot itself

---
+++ src/containers/HubblePlot3DContainer.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import HubblePlot3D from '../components/charts/hubblePlot/HubblePlot3D.jsx';
+
+class HubblePlot3DContainer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/data/galaxies/galaxy_selector.json').then(response => {
+      this.setState(prevState => ({
+        ...prevState,
+        data: response.data,
+      }));
+    });
+  }
+
+  hubbleSelectionCallback = d => {
+    console.log(d); // eslint-disable-line no-console
+    // const { activeQuestionId } = this.props;
+    // if (activeQuestionId) {
+    //   updateAnswer(activeQuestionId, activeSupernova);
+    // }
+  };
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div>
+        <HubblePlot3D
+          className="hubble-plot"
+          {...{
+            data,
+          }}
+          selectionCallback={this.hubbleSelectionCallback}
+        />
+      </div>
+    );
+  }
+}
+
+HubblePlot3DContainer.propTypes = {
+  data: PropTypes.object,
+  options: PropTypes.object,
+  answers: PropTypes.object,
+  activeQuestionId: PropTypes.string,
+  activeAnswer: PropTypes.object,
+  updateAnswer: PropTypes.func,
+};
+
+export default HubblePlot3DContainer;
