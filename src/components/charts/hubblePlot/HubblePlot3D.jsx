7f33084b31ae4564fdb0de04e0481dbf80cf1f20
parkerkaufmann <parkerkaufmann@email.arizona.edu>
2020-01-22 09:58:16 -0700

    [F] Basic HubblePlot3D setup

--- src/components/charts/hubblePlot/HubblePlot3D.jsx
+++ src/components/charts/hubblePlot/HubblePlot3D.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import 'echarts-gl';
+import ReactEcharts from 'echarts-for-react';
 import PropTypes from 'prop-types';
+// import 'echarts';
 
 class HubblePlot3D extends React.PureComponent {
   componentDidMount() {
@@ -12,8 +15,66 @@ class HubblePlot3D extends React.PureComponent {
     console.log('component did update', data, options); // eslint-disable-line no-console
   }
 
+  getAxisInfo(axisName) {
+    return {
+      name: axisName,
+      nameGap: 25,
+      type: 'value',
+      nameTextStyle: {
+        fontSize: 15,
+        fontFamily: 'Roboto',
+      },
+    };
+  }
+
+  getOption(data) {
+    return {
+      grid3D: {},
+      xAxis3D: this.getAxisInfo('Distance'),
+      yAxis3D: this.getAxisInfo('Redshift'),
+      zAxis3D: this.getAxisInfo('Velocity'),
+      dataset: {
+        source: data,
+        dimensions: ['distance', 'redshift', 'velocity'],
+      },
+      series: [
+        {
+          type: 'scatter3D',
+          symbolSize: 10,
+          itemStyle: {
+            color: params => {
+              return params.data.color;
+            },
+          },
+          label: {
+            show: true,
+            distance: 3,
+            formatter: params => {
+              return params.data.name;
+            },
+            textStyle: {
+              fontSize: 10,
+              fontFamily: 'Roboto',
+            },
+          },
+        },
+      ],
+    };
+  }
+
   render() {
-    return <div>I am a 3D Hubble Plot! Or am I?????????</div>;
+    const { data } = this.props;
+
+    return (
+      <>
+        {data && (
+          <ReactEcharts
+            style={{ height: '500px', width: '100%' }}
+            option={this.getOption(data)}
+          />
+        )}
+      </>
+    );
   }
 }
 
