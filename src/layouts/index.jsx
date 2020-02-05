622a62f6d51aa890fe4f8119db621c0598a45bca
Blake Mason <bmason@lsst.org>
2020-02-05 13:29:36 -0800

    [C] Refactor Progress Tracking and add totals

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { graphql, StaticQuery } from 'gatsby';
+import flattenDeep from 'lodash/flattenDeep';
+import filter from 'lodash/filter';
 import GlobalStore from '../state/GlobalStore';
-// import '../assets/stylesheets/styles.scss';
 import SEO from '../components/seo';
 import Header from '../components/site/header';
 import TableOfContents from '../components/site/tableOfContents';
@@ -11,29 +13,74 @@ import styles from './layout.module.scss';
 class Layout extends React.Component {
   constructor(props) {
     super(props);
-    const { data } = this.props;
-
-    const store = new GlobalStore(this.getInvestigationId(data));
-
-    store.addCallbacks();
-    store.addReducers();
+    const { data: allInvestigationsPages, pageContext } = props;
+    const { investigation } = pageContext;
 
     this.state = {
       openSidebar: false,
+      pages: filter(allInvestigationsPages, ['investigation', investigation]),
     };
+
+    const store = new GlobalStore(this.getInitialGlobals());
+    store.addCallbacks();
+    store.addReducers();
   }
 
-  getInvestigationId(data) {
-    let investigationId = 'store';
-    if (data && data.allPagesJson) {
-      const {
-        allPagesJson: { nodes },
-      } = data;
-      const { investigation } = nodes[0];
-      investigationId = investigation || 'store';
-    }
+  getTotalQAs() {
+    const { pages } = this.state;
+    let total = 0;
 
-    return investigationId;
+    pages.forEach(page => {
+      const { questionsByPage } = page;
+      total += questionsByPage ? questionsByPage.length : 0;
+    });
+
+    return { questions: total, answers: 0 };
+  }
+
+  getTotalPages() {
+    const { pages } = this.state;
+    return pages.length;
+  }
+
+  getQIds(questionsByPage) {
+    return flattenDeep(
+      questionsByPage.map(q => {
+        return q.question.map(nested => {
+          return nested.id;
+        });
+      })
+    );
+  }
+
+  getTotalQAsByPage() {
+    const { pages } = this.state;
+    const total = {};
+
+    pages.forEach(page => {
+      const { questionsByPage, id } = page;
+
+      const qIds = questionsByPage ? this.getQIds(questionsByPage) : [];
+      total[id] = {
+        questions: qIds,
+        answers: [],
+        progress: 0,
+      };
+    });
+
+    return total;
+  }
+
+  getInitialGlobals() {
+    const { pageContext } = this.props;
+    const { investigation } = pageContext || {};
+
+    return {
+      investigation,
+      totalPages: this.getTotalPages(),
+      totalQAsByInvestigation: this.getTotalQAs(),
+      totalQAsByPage: this.getTotalQAsByPage(),
+    };
   }
 
   toggleSidebar = () => {
@@ -45,8 +92,9 @@ class Layout extends React.Component {
   };
 
   render() {
-    const { openSidebar } = this.state;
+    const { openSidebar, pages } = this.state;
     const { children, pageContext } = this.props;
+    const { investigation } = pageContext || {};
 
     return (
       <>
@@ -59,21 +107,43 @@ class Layout extends React.Component {
         <TableOfContents
           visible={openSidebar}
           toggleSidebar={this.toggleSidebar}
-          investigation={pageContext.investigation}
+          investigation={investigation}
+          navLinks={pages}
         />
         <div>
           <main className={styles.container}>{children}</main>
         </div>
-        {/* <Footer /> */}
       </>
     );
   }
 }
 
-export default Layout;
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query MyQuery {
+        allPagesJson(sort: { fields: order, order: ASC }) {
+          nodes {
+            title
+            slug
+            id
+            investigation
+            order
+            questionsByPage {
+              question {
+                id
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Layout {...props} data={data.allPagesJson.nodes} />}
+  />
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
   pageContext: PropTypes.object,
-  data: PropTypes.object,
+  data: PropTypes.array,
 };
