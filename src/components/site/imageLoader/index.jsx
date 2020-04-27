a04c81fe31d8682341286715318cba2a47513a6e
parkerkaufmann <parkerkaufmann@email.arizona.edu>
2020-04-27 12:58:21 -0700

    [F] Add ImageLoader for lazy loading ImageBlocks
    EPO-2701

---
+++ src/components/site/imageLoader/index.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import CircularProgress from 'react-md/lib//Progress/CircularProgress';
+import {
+  imageLoaded,
+  imageNotLoaded,
+  loaderImage,
+} from './image-loader.module.scss';
+
+class ImageLoader extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    };
+  }
+
+  handleLoading = () => {
+    this.setState(() => ({ loaded: true }));
+  };
+
+  render() {
+    const { src, alt } = this.props;
+    const { loaded } = this.state;
+    const classes = classnames(loaderImage, {
+      [imageLoaded]: loaded,
+      [imageNotLoaded]: !loaded,
+    });
+
+    return (
+      <>
+        {!loaded && (
+          <CircularProgress
+            id="img-loader"
+            className="chart-loader"
+            scale={3}
+          />
+        )}
+        <img
+          className={classes}
+          src={src}
+          alt={alt}
+          onLoad={this.handleLoading}
+        />
+      </>
+    );
+  }
+}
+
+ImageLoader.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+};
+
+export default ImageLoader;
