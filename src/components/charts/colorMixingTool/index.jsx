97b1389b1ea5c776e3092b2e7c0f71e77a55d240
parkerkaufmann <parkerkaufmann@email.arizona.edu>
2020-07-09 17:14:21 -0700

    [F] Button Toggle Image On/Off
    EPO-3315

--- src/components/charts/colorMixingTool/index.jsx
+++ src/components/charts/colorMixingTool/index.jsx
@@ -1,39 +1,73 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../../site/button/index.js';
-import { groupLayout, colorImage } from './color-tool.module.scss';
-import picture from '../../../../static/images/bayer_filter.png';
+import { redBtn } from '../../site/button/styles.module.scss';
+import {
+  groupLayout,
+  colorImage,
+  button,
+  buttonClicked,
+} from './color-tool.module.scss';
+import greenPic from '../../../../static/images/green-filter.png';
+import redPic from '../../../../static/images/red-filter.png';
+import bluePic from '../../../../static/images/blue-filter.png';
 
 class ColorTool extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       showImage: false,
+      imgSrc: null,
+      greenIsClick: false,
+      redIsClick: false,
+      blueIsClick: false,
     };
   }
 
-  handleImage = () => {
+  handleImage(color, clickValue) {
+    const src =
+      color === 'red' ? redPic : color === 'blue' ? bluePic : greenPic;
     this.setState(prevState => ({
       ...prevState,
       showImage: true,
+      imgSrc: src,
+      [clickValue]: !prevState[clickValue],
     }));
-  };
+  }
 
   render() {
     const { options } = this.props;
-    const { showImage } = this.state;
-    console.log(showImage);
+    const {
+      showImage,
+      imgSrc,
+      greenIsClick,
+      redIsClick,
+      blueIsClick,
+    } = this.state;
+
     return (
-      <div className={groupLayout}>
-        {showImage && <img className={colorImage} alt="img" src={picture} />}
+      <div>
+        {showImage && <img className={colorImage} alt="img" src={imgSrc} />}
         <div>
-          <Button floating onClick={this.handleImage}>
+          <Button
+            floating
+            className={redIsClick ? buttonClicked : button}
+            onClick={() => this.handleImage('red', 'redIsClick')}
+          >
             R
           </Button>
-          <Button floating onClick={this.handleImage}>
+          <Button
+            floating
+            className={blueIsClick ? buttonClicked : button}
+            onClick={() => this.handleImage('blue', 'blueIsClick')}
+          >
             B
           </Button>
-          <Button floating onClick={this.handleImage}>
+          <Button
+            floating
+            className={greenIsClick ? buttonClicked : button}
+            onClick={() => this.handleImage('green', 'greenIsClick')}
+          >
             G
           </Button>
         </div>
