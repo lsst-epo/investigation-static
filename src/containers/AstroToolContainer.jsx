80857bb36a1561f36145c92dce7bd57ec046e426
parkerkaufmann <parkerkaufmann@email.arizona.edu>
2020-07-23 12:49:53 -0700

    [WIP]
    EPO-3434
    EPO-3437

---
+++ src/containers/AstroToolContainer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ColorTool from '../components/charts/colorMixingTool/index.jsx';
+import API from '../lib/API.js';
+
+class AstroToolContainer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    const { widget } = this.props;
+    const { source } = widget;
+
+    API.get(source).then(response => {
+      const { data } = response;
+      this.setState(prevState => ({
+        ...prevState,
+        data,
+      }));
+    });
+  }
+
+  render() {
+    const { data } = this.state;
+    const { filters, colorOptions, hexColors, galaxies } = data || {};
+
+    return (
+      data && (
+        <ColorTool
+          useOverlay={true}
+          {...{ filters, colorOptions, hexColors, galaxies }}
+        />
+      )
+    );
+  }
+}
+
+AstroToolContainer.propTypes = {
+  widget: PropTypes.object,
+};
+
+export default AstroToolContainer;
