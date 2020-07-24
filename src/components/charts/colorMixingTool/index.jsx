a4279c55f1c1182c6885b73653220f18d2c40019
parkerkaufmann <parkerkaufmann@email.arizona.edu>
2020-07-23 19:59:30 -0700

    [F] Control Panel Overlay
    EPO-3438

--- src/components/charts/colorMixingTool/index.jsx
+++ src/components/charts/colorMixingTool/index.jsx
@@ -30,25 +30,18 @@ class ColorTool extends React.PureComponent {
       hexColors: [],
       galaxies: null,
       resetBtnActive: false,
-      useOverlay: false,
+      panelIsActive: true,
     };
   }
 
   componentDidMount() {
-    const {
-      filters,
-      colorOptions,
-      hexColors,
-      useOverlay,
-      galaxies,
-    } = this.props;
+    const { filters, colorOptions, hexColors, galaxies } = this.props;
 
     this.setState(prevState => ({
       ...prevState,
       filters,
       colorOptions,
       hexColors,
-      useOverlay,
       galaxies,
       resetBtnActive: false,
     }));
@@ -150,7 +143,6 @@ class ColorTool extends React.PureComponent {
     const { filters } = this.state;
 
     const newFilters = filters.map(newFilter => {
-      newFilter.selectedGalaxy = value;
       newFilter.image = `${value.toLowerCase()}-${newFilter.label.toLowerCase()}.png`;
       return newFilter;
     });
@@ -164,111 +156,114 @@ class ColorTool extends React.PureComponent {
   handleControlPanel = () => {
     this.setState(prevState => ({
       ...prevState,
-      useOverlay: !prevState.useOverlay,
+      panelIsActive: !prevState.panelIsActive,
     }));
   };
 
   render() {
-    const { filters, resetBtnActive, useOverlay, galaxies } = this.state;
-    console.log(filters);
+    const { filters, resetBtnActive, panelIsActive, galaxies } = this.state;
+    const panelClassName = classnames(button, {
+      [active]: panelIsActive,
+    });
+    console.log(panelIsActive);
+
     return (
-      <div>
-        {galaxies && filters && (
-          <div className={container}>
-            <Select
-              dropdownIcon={<ArrowDown />}
-              id="galaxy-selector"
-              placeholder="None"
-              menuItems={galaxies}
-              onChange={this.handleGalaxySelection}
-            />
-            <Button
-              floating
-              className={button}
-              onClick={this.handleControlPanel}
-            >
-              {useOverlay ? 'On' : 'Off'}
-            </Button>
-          </div>
-        )}
-        <div className={`container-flex ${container}`}>
-          {!useOverlay && (
-            <div className={`${buttonContainer} ${col50}`}>
-              {filters &&
-                filters.map((btn, i) => {
-                  const key = `div-${i}`;
-                  const btnClassName = classnames(button, {
-                    [active]: btn.active,
-                  });
-                  return (
-                    <div key={key} className={selectContainer}>
-                      <Button
-                        floating
-                        className={btnClassName}
-                        onClick={() => this.handleImage(btn)}
-                      >
-                        {btn.label}
-                      </Button>
-                      <Select
-                        dropdownIcon={<ArrowDown />}
-                        id={btn.label}
-                        placeholder="None"
-                        value={btn.color}
-                        menuItems={this.getColorBlocks()}
-                        onChange={this.handleColorChange}
-                      />
-                      <div className={sliderContainer}>
-                        <SliderCustom
-                          id={btn.label}
-                          className={
-                            btn.color !== ''
-                              ? `fill-color-${btn.color.toLowerCase()}`
-                              : ''
-                          }
-                          min={0.7}
-                          max={1.5}
-                          value={btn.brightness || 0.7}
-                          disabled={!btn.active}
-                          onChange={value =>
-                            this.handleBrightness(value, btn.label)
-                          }
-                        />
-                      </div>
-                    </div>
-                  );
-                })}
-              <Button
-                raised
-                primary
-                disabled={!resetBtnActive}
-                className={resetBtn}
-                onClick={this.handleReset}
-              >
-                Reset
-              </Button>
+      <div className={`container-flex ${container}`}>
+        <div className={`${buttonContainer} ${col50}`}>
+          {galaxies && filters && (
+            <div className={container}>
+              <Select
+                dropdownIcon={<ArrowDown />}
+                id="galaxy-selector"
+                placeholder="Selected Galaxy:"
+                menuItems={galaxies}
+                onChange={this.handleGalaxySelection}
+              />
             </div>
           )}
-          <div className={`${imagesContainer} ${col50}`}>
-            {filters &&
-              filters.map(filterImage => {
-                const imageClassName = classnames(filter, {
-                  [filterActive]: filterImage.active,
-                });
-
-                return (
-                  <div
-                    key={`filter-${filterImage.label}`}
-                    style={{
-                      backgroundImage: `url(/images/${filterImage.image}`,
-                      backgroundColor: filterImage.color,
-                      filter: `brightness(${filterImage.brightness})`,
-                    }}
-                    className={imageClassName}
-                  ></div>
-                );
-              })}
-          </div>
+          <Button
+            floating
+            className={panelClassName}
+            onClick={this.handleControlPanel}
+          >
+            {panelIsActive ? 'Off' : 'On'}
+          </Button>
+          {filters &&
+            panelIsActive &&
+            filters.map((btn, i) => {
+              const key = `div-${i}`;
+              const btnClassName = classnames(button, {
+                [active]: btn.active,
+              });
+              return (
+                <div key={key} className={selectContainer}>
+                  <Button
+                    floating
+                    className={btnClassName}
+                    onClick={() => this.handleImage(btn)}
+                  >
+                    {btn.label}
+                  </Button>
+                  <Select
+                    dropdownIcon={<ArrowDown />}
+                    id={btn.label}
+                    placeholder="None"
+                    value={btn.color}
+                    menuItems={this.getColorBlocks()}
+                    onChange={this.handleColorChange}
+                  />
+                  <div className={sliderContainer}>
+                    <SliderCustom
+                      id={btn.label}
+                      className={
+                        btn.color !== ''
+                          ? `fill-color-${btn.color.toLowerCase()}`
+                          : ''
+                      }
+                      min={0.7}
+                      max={1.5}
+                      value={btn.brightness || 0.7}
+                      disabled={!btn.active}
+                      onChange={value =>
+                        this.handleBrightness(value, btn.label)
+                      }
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          {panelIsActive && (
+            <Button
+              raised
+              primary
+              disabled={!resetBtnActive}
+              className={resetBtn}
+              onClick={this.handleReset}
+            >
+              Reset
+            </Button>
+          )}
         </div>
+        {filters &&
+          filters.map(filterImage => {
+            const imageClassName = classnames(filter, {
+              [filterActive]: filterImage.active,
+            });
+            const opacityVal = panelIsActive ? 0.3 : 1;
+
+            return (
+              <div
+                key={`filter-${filterImage.label}`}
+                style={{
+                  opacity: `${opacityVal}`,
+                  backgroundImage: `url(/images/${filterImage.image}`,
+                  backgroundColor: filterImage.color,
+                  filter: `brightness(${filterImage.brightness})`,
+                }}
+                className={imageClassName}
+              ></div>
+            );
+          })}
       </div>
     );
   }
@@ -278,7 +273,6 @@ ColorTool.propTypes = {
   filters: PropTypes.array,
   colorOptions: PropTypes.array,
   hexColors: PropTypes.array,
-  useOverlay: PropTypes.bool,
   galaxies: PropTypes.array,
 };
 
