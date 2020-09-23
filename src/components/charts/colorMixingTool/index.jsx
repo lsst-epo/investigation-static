4adb3792b54b3a16adbcbce293acf761eec6e28e
georgegomez144 <george144k.dev@gmail.com>
2020-09-23 09:25:54 -0700

    [F] Add boolean to ColorTool to show controls or image
    Includes copy and layout updates to page 17
    Includes update to json data and allow buttons to have either upper or lowercase

--- src/components/charts/colorMixingTool/index.jsx
+++ src/components/charts/colorMixingTool/index.jsx
@@ -23,7 +23,8 @@ import {
   button,
   active,
   col50,
-  buttonContainer,
+  colFullWidth,
+  controlsContainer,
   sliderContainer,
   colorToolSelect,
   container,
@@ -181,6 +182,8 @@ class ColorTool extends React.PureComponent {
       hexColors,
       colorOptions,
       toolIsInteractable,
+      hideControls,
+      hideImage,
     } = this.props;
     const { data, selectedData, resetBtnActive, selectorValue } = this.state;
     const widgetTitle =
@@ -188,134 +191,145 @@ class ColorTool extends React.PureComponent {
         ? `${title}: ${objectName || selectorValue}`
         : title;
     const filters = selectedData ? selectedData.filters : [];
+    const controlsContainerClasses = classnames(container, controlsContainer, {
+      [col50]: !hideControls && !hideImage,
+    });
+    const imageContainerClasses = classnames(imageContainer, {
+      [col50]: !hideControls && !hideImage,
+      [colFullWidth]: hideControls && !hideImage,
+    });
 
     return (
       <>
         {title && <h2>{widgetTitle}</h2>}
         <div className={`container-flex ${container}`}>
-          <div className={`${buttonContainer} ${col50}`}>
-            {isArray(data) && (
-              <div className={`${container} ${selectToolContainer}`}>
-                <Select
-                  className={colorToolSelect}
-                  dropdownIcon={<ArrowDown />}
-                  id="galaxy-selector"
-                  placeholder="Select An Object"
-                  menuItems={menuItems}
-                  onChange={this.handleGalaxySelection}
-                  value={selectorValue}
-                  saveListScrollTop
-                  fullWidth
-                  listStyle={{ width: '100%' }}
-                  // position="below"
-                />
-              </div>
-            )}
-            {filters && (
-              <div className={toolActionsHeader}>
-                <div className={buttonToolContainer}>Filter</div>
-                <div className={colorSwatchContainer}></div>
-                <div className={selectToolContainer}>Color</div>
-                <div className={sliderToolContainer}>Color Intensity</div>
-              </div>
-            )}
-            {filters &&
-              filters.map((btn, i) => {
-                const key = `div-${i}`;
-                const btnClassName = classnames(button, {
-                  [active]: btn.active,
-                });
+          {!hideControls && (
+            <div className={controlsContainerClasses}>
+              {isArray(data) && (
+                <div className={`${container} ${selectToolContainer}`}>
+                  <Select
+                    className={colorToolSelect}
+                    dropdownIcon={<ArrowDown />}
+                    id="galaxy-selector"
+                    placeholder="Select An Object"
+                    menuItems={menuItems}
+                    onChange={this.handleGalaxySelection}
+                    value={selectorValue}
+                    saveListScrollTop
+                    fullWidth
+                    listStyle={{ width: '100%' }}
+                    // position="below"
+                  />
+                </div>
+              )}
+              {filters && (
+                <div className={toolActionsHeader}>
+                  <div className={buttonToolContainer}>Filter</div>
+                  <div className={colorSwatchContainer}></div>
+                  <div className={selectToolContainer}>Color</div>
+                  <div className={sliderToolContainer}>Color Intensity</div>
+                </div>
+              )}
+              {filters &&
+                filters.map((btn, i) => {
+                  const key = `div-${i}`;
+                  const btnClassName = classnames(button, {
+                    [active]: btn.active,
+                  });
 
-                return (
-                  <div key={key} className={selectContainer}>
-                    <div className={buttonToolContainer}>
-                      <Button
-                        floating
-                        disabled={!toolIsInteractable || btn.isDisabled}
-                        className={btnClassName}
-                        onClick={() => this.handleImage(btn)}
-                      >
-                        {btn.label}
-                      </Button>
+                  return (
+                    <div key={key} className={selectContainer}>
+                      <div className={buttonToolContainer}>
+                        <Button
+                          floating
+                          disabled={!toolIsInteractable || btn.isDisabled}
+                          className={btnClassName}
+                          onClick={() => this.handleImage(btn)}
+                        >
+                          {btn.label}
+                        </Button>
+                      </div>
+                      <div className={selectToolContainer}>
+                        <Select
+                          className={colorToolSelect}
+                          dropdownIcon={<ArrowDown />}
+                          disabled={!toolIsInteractable || btn.isDisabled}
+                          id={`${btn.label}-filter`}
+                          placeholder="Select a filter"
+                          value={btn.color}
+                          menuItems={colorBlocks}
+                          onChange={this.handleColorChange}
+                          position="top left"
+                          fullWidth
+                          simplifiedMenu={false}
+                          listStyle={{
+                            left: '50%',
+                            top: '-50%',
+                            width: '100%',
+                            minWidth: '150px',
+                            transform: 'translateY(-30%)',
+                          }}
+                        />
+                      </div>
+                      <div className={sliderContainer}>
+                        <SliderCustom
+                          id={btn.label}
+                          className={getColorNameFromHex(
+                            btn.color,
+                            hexColors,
+                            colorOptions
+                          )}
+                          min={1}
+                          max={100}
+                          value={btn.value || 1}
+                          disabled={!toolIsInteractable || !btn.active}
+                          onChange={value =>
+                            this.handleBrightness(value, btn.label)
+                          }
+                          discrete
+                        />
+                      </div>
                     </div>
-                    <div className={selectToolContainer}>
-                      <Select
-                        className={colorToolSelect}
-                        dropdownIcon={<ArrowDown />}
-                        disabled={!toolIsInteractable || btn.isDisabled}
-                        id={`${btn.label}-filter`}
-                        placeholder="Select a filter"
-                        value={btn.color}
-                        menuItems={colorBlocks}
-                        onChange={this.handleColorChange}
-                        position="top left"
-                        fullWidth
-                        simplifiedMenu={false}
-                        listStyle={{
-                          left: '50%',
-                          top: '-50%',
-                          width: '100%',
-                          minWidth: '150px',
-                          transform: 'translateY(-30%)',
-                        }}
-                      />
-                    </div>
-                    <div className={sliderContainer}>
-                      <SliderCustom
-                        id={btn.label}
-                        className={getColorNameFromHex(
-                          btn.color,
-                          hexColors,
-                          colorOptions
-                        )}
-                        min={1}
-                        max={100}
-                        value={btn.value || 1}
-                        disabled={!toolIsInteractable || !btn.active}
-                        onChange={value =>
-                          this.handleBrightness(value, btn.label)
-                        }
-                        discrete
-                      />
-                    </div>
-                  </div>
-                );
-              })}
-            {selectorValue && (
-              <Button
-                raised
-                primary
-                disabled={!toolIsInteractable || !resetBtnActive}
-                className={resetBtn}
-                onClick={this.handleReset}
-              >
-                Reset
-              </Button>
-            )}
-          </div>
-          <div className={`${imageContainer} ${col50}`}>
-            {filters &&
-              filters.map(filterImg => {
-                const { label, image, color, brightness } = filterImg;
-                const imageClassName = classnames(filterImage, {
-                  [filterActive]: filterImg.active,
-                });
+                  );
+                })}
+              {selectorValue && (
+                <Button
+                  raised
+                  primary
+                  disabled={!toolIsInteractable || !resetBtnActive}
+                  className={resetBtn}
+                  onClick={this.handleReset}
+                >
+                  Reset
+                </Button>
+              )}
+            </div>
+          )}
+          {!hideImage && (
+            <div className={imageContainerClasses}>
+              {filters &&
+                filters.map(filterImg => {
+                  const { label, image, color, brightness } = filterImg;
+                  const imageClassName = classnames(filterImage, {
+                    [filterActive]: filterImg.active,
+                  });
 
-                return (
-                  <div
-                    key={`filter-${label}`}
-                    className={imageClassName}
-                    style={{
-                      backgroundImage: image
-                        ? `url(/images/colorTool/${image}`
-                        : 'none',
-                      backgroundColor: color,
-                      filter: `brightness(${brightness}) contrast(1.3)`,
-                    }}
-                  ></div>
-                );
-              })}
-          </div>
+                  return (
+                    <div
+                      key={`filter-${label}`}
+                      className={imageClassName}
+                      style={{
+                        backgroundImage: image
+                          ? `url(/images/colorTool/${image}`
+                          : 'none',
+                        backgroundColor: color,
+                        filter: `brightness(${brightness}) contrast(1.3)`,
+                      }}
+                    ></div>
+                  );
+                })}
+            </div>
+          )}
         </div>
       </>
     );
@@ -324,6 +338,8 @@ class ColorTool extends React.PureComponent {
 
 ColorTool.defaultProps = {
   toolIsInteractable: true,
+  hideControls: false,
+  hideImage: false,
 };
 
 ColorTool.propTypes = {
@@ -339,6 +355,8 @@ ColorTool.propTypes = {
   objectName: PropTypes.string,
   hasQuestionId: PropTypes.bool,
   toolIsInteractable: PropTypes.bool,
+  hideControls: PropTypes.bool,
+  hideImage: PropTypes.bool,
 };
 
 export default ColorTool;
