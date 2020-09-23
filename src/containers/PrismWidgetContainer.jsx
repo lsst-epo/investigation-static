cbb5f7eb87eb6e51c7cd4433b8b66b99fef54e03
georgegomez144 <george144k.dev@gmail.com>
2020-09-23 08:24:23 -0700

    [F] Prism widget will store answer
    Initial setup
    
    [F] Prism Tool to save selected color and add labels
    
    Includes:
    - Add label to 'filter'
    - Add label to color select
    - Prism Tool can store selected color when questionId present

--- src/containers/PrismWidgetContainer.jsx
+++ src/containers/PrismWidgetContainer.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import PrismWidget from '../components/charts/prismWidget/index.jsx';
 
 class PrismWidgetContainer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedColor: null,
+    };
+  }
+
+  componentDidMount() {
+    const { widget } = this.props;
+    const { options } = widget;
+    const { questionId } = options || {};
+    const { answers } = this.props;
+    const answer = answers[questionId];
+    const { data: answerData } = answer || {};
+
+    this.setState(prevState => ({
+      ...prevState,
+      selectedColor: answerData || 'None',
+    }));
+  }
+
+  selectionCallback = value => {
+    const { widget } = this.props;
+    const { options } = widget;
+    const { questionId } = options || {};
+    const { updateAnswer } = this.props;
+    if (!value) return;
+
+    if (questionId) {
+      updateAnswer(questionId, value);
+      this.setState(prevState => ({
+        ...prevState,
+        selectedColor: value,
+      }));
+    }
+  };
+
   render() {
-    return <PrismWidget />;
+    const { selectedColor } = this.state;
+    const { widget } = this.props;
+    const {
+      options: { questionId },
+    } = widget || {};
+
+    return (
+      selectedColor && (
+        <PrismWidget
+          selectedColor={selectedColor}
+          hasQuestionId={questionId !== null}
+          selectionCallback={this.selectionCallback}
+        />
+      )
+    );
   }
 }
+
+PrismWidgetContainer.propTypes = {
+  widget: PropTypes.object,
+  answers: PropTypes.object,
+  updateAnswer: PropTypes.func,
+};
 
 export default PrismWidgetContainer;
