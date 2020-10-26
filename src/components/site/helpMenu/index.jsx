9b4178f56737914990a7b8c525b384ba0f80109f
parkerkaufmann <parkerkaufmann@email.arizona.edu>
2020-10-26 15:23:57 -0700

    [F] Help Menu for Widgets
    EPO-2744

--- src/components/site/helpMenu/index.jsx
+++ src/components/site/helpMenu/index.jsx
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import { Button, DialogContainer } from 'react-md';
 import ButtonIcon from '../button/ButtonIcon';
 import InfoIcon from '../icons/InfoIcon';
+import { helpBtn } from './help-menu.module.scss';
 
 export default class SimpleModal extends PureComponent {
   constructor(props) {
@@ -29,11 +30,13 @@ export default class SimpleModal extends PureComponent {
 
     return (
       <div>
-        <Button
-          icon
-          iconEl={<ButtonIcon srText="Help" Icon={InfoIcon} />}
-          onClick={this.handleShow}
-        />
+        <div className={helpBtn}>
+          <Button
+            icon
+            iconEl={<ButtonIcon srText="Help" Icon={InfoIcon} />}
+            onClick={this.handleShow}
+          />
+        </div>
         <DialogContainer
           id="simple-action-dialog"
           visible={visible}
