d42a1d33d1f50064a7f7b12eed86f198957f3210
georgegomez144 <george144k.dev@gmail.com>
2021-03-18 14:49:56 -0700

    [F] Add AnswerRequiredAlert component

---
+++ src/components/site/answerRequiredAlert/index.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import CloseIcon from '../icons/Close.jsx';
+import StopIcon from '../icons/Stop.jsx';
+import {
+  answerRequiredInner,
+  answerRequired,
+  closeButton,
+  showAnswerRequired,
+  stopIcon,
+  divider,
+} from './answer-required.module.scss';
+
+const AnswerRequiredAlert = ({ showAlert, handleClose }) => {
+  const alertTimeout = useRef(null);
+
+  useEffect(() => {
+    if (showAlert) {
+      alertTimeout.current = setTimeout(handleClose, 5000);
+    } else {
+      clearTimeout(alertTimeout.current);
+    }
+  }, [showAlert]);
+
+  const alertClasses = classnames(answerRequired, {
+    [showAnswerRequired]: showAlert,
+  });
+
+  return (
+    <div className={alertClasses}>
+      <button type="button" className={closeButton} onClick={handleClose}>
+        <CloseIcon />
+      </button>
+      <div className={answerRequiredInner}>
+        <StopIcon className={stopIcon} />
+        <div className={divider} />
+        <p>Please answer all questions before continuing to the next page.</p>
+      </div>
+    </div>
+  );
+};
+
+AnswerRequiredAlert.propTypes = {
+  showAlert: PropTypes.bool,
+  handleClose: PropTypes.func,
+};
+
+export default AnswerRequiredAlert;
