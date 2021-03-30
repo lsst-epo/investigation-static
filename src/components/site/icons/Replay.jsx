a03aea11093bda4cccfd440b7f7aa60cc449a48e
georgegomez144 <george144k.dev@gmail.com>
2021-03-30 15:59:24 -0700

    [F] Improve OrbitViewer controls usability

---
+++ src/components/site/icons/Replay.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+import React from 'react';
+import SVGIcon from 'react-md/lib/SVGIcons/SVGIcon';
+import './icon.module.scss';
+
+const Replay = props => (
+  <SVGIcon {...props} role="presentation">
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z" />
+  </SVGIcon>
+);
+
+export default Replay;
