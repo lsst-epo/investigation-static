066467888f5d548a63fd17d29eb5c9329522ac2c
Blake Mason <bmason@lsst.org>
2021-04-22 20:42:58 -0700

    [C] Disable accessibility linter/checker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import axe from 'react-axe';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import axe from 'react-axe';
 
 import './src/assets/stylesheets/styles.scss';
 
-export const onInitialClientRender = () => {
-  if (process.env.NODE_ENV === 'development') {
-    axe(React, ReactDOM, 1000);
-  }
-};
+// export const onInitialClientRender = () => {
+//   if (process.env.NODE_ENV === 'development') {
+//     axe(React, ReactDOM, 1000);
+//   }
+// };
