3675ed79b75e71e0a23324da2cd5474f60da815d
Blake Mason <bmason@lsst.org>
2021-07-27 11:40:19 -0700

    [C] Additional HazAst content revisions based on user testing

--- src/components/qas/questions/qaCalculator/equations/FindMass.jsx
+++ src/components/qas/questions/qaCalculator/equations/FindMass.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
 import PropTypes from 'prop-types';
+// import Tex2SVG from 'react-hook-mathjax';
 import MathJax from '../../../../site/mathJax';
 import {
   addTheCommas,
@@ -14,24 +15,60 @@ import {
 
 export default function FindMass(props) {
   const { density, mass, diameter } = props;
+  //, velocity, kineticEnergy
+  function getDensityChar(d) {
+    if (!d) {
+      return colorize(`\\rho`);
+    }
 
-  const p = !density
-    ? colorize(`\\rho`)
-    : addColorAndUnit(addTheCommas(density), 'density');
-  const d = !diameter
-    ? colorize(`D_a`)
-    : addColorAndUnit(addTheCommas(diameter), 'diameter');
-  const m =
-    !mass || +mass === 0
-      ? colorize(`?`)
-      : addColorAndUnit(scientificNotation(mass, 3, 'latex'), 'mass');
+    return addColorAndUnit(addTheCommas(d), 'density');
+  }
 
+  function getDiameterChar(d) {
+    if (!d) {
+      return colorize(`D_a`);
+    }
+
+    return addColorAndUnit(addTheCommas(diameter), 'diameter');
+  }
+
+  function getMassChar(m) {
+    if (!m || +m === 0) {
+      return colorize(`?`);
+    }
+
+    return addColorAndUnit(scientificNotation(mass, 3, 'latex'), 'mass');
+  }
+
+  // function getVelocityChar(v) {
+  //   if (!v) {
+  //     return colorize(`?`);
+  //   }
+
+  //   return addColorAndUnit(v, 'velocity');
+  // }
+
+  // function getkineticEnergyChar(ke) {
+  //   if (!ke) {
+  //     return colorize(`?`);
+  //   }
+
+  //   return addColorAndUnit(scientificNotation(ke, 3, 'latex'), 'kineticEnergy');
+  // }
+
+  const p = getDensityChar(density);
+  const d = getDiameterChar(diameter);
+  const m = getMassChar(mass);
+  // const v = getVelocityChar(velocity);
+  // const ke = getkineticEnergyChar(kineticEnergy);
+
+  // <Tex2SVG
+  //   latex={`{\\bf KE_a} = {1 \\over 2} \\times ${m} \\times (${v}) ^ 2 = \\ ${ke}`}
+  // />
   return (
-    <div>
-      <MathJax
-        laTex={`{\\bf m_a} = ${p} \\  \\times {4 \\over 3} \\times \\pi \\times ({${d} \\  \\over 2}) ^ 3 = \\ ${m}`}
-      />
-    </div>
+    <MathJax
+      latex={`{\\bf m_a} = ${p} \\  \\times {4 \\over 3} \\times \\pi \\times ({${d} \\  \\over 2}) ^ 3 = \\ ${m}`}
+    />
   );
 }
 
