3c33b8c67a022e96016224a4b67c1c52e5b9edb9
Blake Mason <bmason@lsst.org>
2021-09-21 09:35:44 -0700

    [C] Update Hazardous Asteroids Acknowledgements & checkpoints
    Includes minor updates to all Acknowledgements page titles

--- src/components/site/icons/SkipForward.jsx
+++ src/components/site/icons/SkipForward.jsx
@@ -6,8 +6,8 @@ import './icon.module.scss';
 
 const SkipForward = props => (
   <SVGIcon {...props} role="presentation">
-    <path d="M0 0h24v24H0V0z" fill="none"/>
-    <path d="M6 18l8.5-6L6 6v12zm2-8.14L11.03 12 8 14.14V9.86zM16 6h2v12h-2z"/>
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path d="M6 18l8.5-6L6 6v12zm2-8.14L11.03 12 8 14.14V9.86zM16 6h2v12h-2z" />
   </SVGIcon>
 );
 
