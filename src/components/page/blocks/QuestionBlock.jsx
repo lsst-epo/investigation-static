9cfd34bb55ef02940086c347eeb185dd878de6aa
Alexandra Goff <agoff@lsst.org>
2022-02-22 14:34:51 -0700

    [C] Add layout options to questions
    For JIRA: "EPO-5885 #IN-REVIEW #comment add layout options to questions"

--- src/components/page/blocks/QuestionBlock.jsx
+++ src/components/page/blocks/QuestionBlock.jsx
@@ -20,7 +20,10 @@ class QuestionBlock extends React.PureComponent {
 
   render() {
     const { blockShared, block } = this.props;
-    const { questions } = block;
+    const { question: q, number, tables } = block;
+    const primeQ = q[0];
+    const { id, questionType, qaReview, showUserAnswer } = primeQ;
+    const key = `qa-${id}`;
 
     const {
       activeQuestionId,
@@ -31,50 +34,43 @@ class QuestionBlock extends React.PureComponent {
       updateAnswer,
     } = blockShared;
 
+    if (qaReviewPage && !qaReview) return false;
+
+    if (q.length > 1) {
+      return (
+        <div className="qas">
+          <div className={qa} key={key}>
+            <QACompound
+              activeId={activeQuestionId}
+              questionNumber={+number}
+              questions={q}
+              tables={tables}
+              answers={answers}
+              handleAnswerSelect={updateAnswer}
+            />
+          </div>
+        </div>
+      );
+    }
+
+    const answer = answers[id];
+    const prepopulateAnswer = answers[showUserAnswer];
+
     return (
       <div className="qas">
-        {questions.map(question => {
-          const { question: q, number, tables } = question;
-          const primeQ = q[0];
-          const { id, questionType, qaReview, showUserAnswer } = primeQ;
-          const key = `qa-${id}`;
-
-          if (qaReviewPage && !qaReview) return false;
-
-          if (q.length > 1) {
-            return (
-              <div className={qa} key={key}>
-                <QACompound
-                  activeId={activeQuestionId}
-                  questionNumber={+number}
-                  questions={q}
-                  tables={tables}
-                  answers={answers}
-                  handleAnswerSelect={updateAnswer}
-                />
-              </div>
-            );
-          }
-
-          const answer = answers[id];
-          const prepopulateAnswer = answers[showUserAnswer];
-
-          return (
-            <QA
-              key={key}
-              questionType={questionType}
-              questionNumber={+number}
-              question={primeQ}
-              answer={answer}
-              prepopulateAnswer={prepopulateAnswer}
-              activeId={activeQuestionId}
-              answerHandler={updateAnswer}
-              cancelHandler={updateAnswer}
-              saveHandler={advanceActiveQuestion}
-              editHandler={setActiveQuestion}
-            />
-          );
-        })}
+        <QA
+          key={key}
+          questionType={questionType}
+          questionNumber={+number}
+          question={primeQ}
+          answer={answer}
+          prepopulateAnswer={prepopulateAnswer}
+          activeId={activeQuestionId}
+          answerHandler={updateAnswer}
+          cancelHandler={updateAnswer}
+          saveHandler={advanceActiveQuestion}
+          editHandler={setActiveQuestion}
+        />
       </div>
     );
   }
