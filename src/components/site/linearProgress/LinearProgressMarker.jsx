41c3dc7585088b5ba89c3810dbd21814fc62f813
Alexandra Goff <agoff@lsst.org>
2022-03-04 09:40:54 -0700

    [F] Finish section break implementation

--- src/components/site/linearProgress/LinearProgressMarker.jsx
+++ src/components/site/linearProgress/LinearProgressMarker.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const LinearProgressMarker = ({ progress, completed, hoverable, children }) => (
+const LinearProgressMarker = ({
+  progress,
+  completed,
+  hoverable,
+  filled,
+  children,
+  style,
+}) => (
   <div
-    className={classnames('progress-linear-marker', { completed, hoverable })}
-    style={{ left: `${progress}%` }}
+    className={classnames('progress-linear-marker', {
+      completed,
+      hoverable,
+      filled,
+    })}
+    style={{ left: `${progress}%`, ...style }}
   >
     {children}
   </div>
@@ -15,7 +26,9 @@ LinearProgressMarker.propTypes = {
   progress: PropTypes.number,
   completed: PropTypes.bool,
   hoverable: PropTypes.bool,
+  filled: PropTypes.bool,
   children: PropTypes.node,
+  style: PropTypes.object,
 };
 
 export default LinearProgressMarker;
