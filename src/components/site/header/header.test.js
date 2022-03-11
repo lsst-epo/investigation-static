3e3a5008b0fbb2f3351a4c9cb5eee2a7fcb4f92f
Alexandra Goff <agoff@lsst.org>
2022-03-11 13:01:33 -0700

    [C] Fix header unit tests

--- src/components/site/header/header.test.js
+++ src/components/site/header/header.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import { render, fireEvent } from '@testing-library/react';
 import Header from './index.jsx';
 
 const testTitle = 'Test Title';
 const handleToggleClick = jest.fn();
+
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  fallbackLng: 'en',
+
+  // have a common namespace used around the full app
+  ns: ['interface'],
+  defaultNS: 'inface',
+
+  debug: true,
+
+  interpolation: {
+    escapeValue: false, // not needed for react!!
+  },
+
+  resources: {
+    en: {
+      interface: {
+        header: { toc_close: 'Close Table of Contents' },
+        locations: { table_of_contents: 'Table of Contents' },
+      },
+    },
+  },
+});
 
 test('Header renders without props', () => {
   // Arrange
@@ -13,29 +39,16 @@ test('Header renders without props', () => {
 });
 
 test('Header Logo alt text is Investigation Title', () => {
+  const headerKey = 'interface::header.title';
   // Arrange
   const { getByAltText, getByText, queryByText } = render(
     <Header logo="fake/image/path" investigationTitle={testTitle} />
   );
   const logoLink = getByAltText(testTitle);
   // Assert
-  expect(getByText(testTitle)).toBeInTheDocument();
+  expect(getByText(headerKey)).toBeInTheDocument();
   expect(queryByText(`${testTitle}: Page`)).toBe(null);
   expect(logoLink).toBeInTheDocument();
-});
-
-test('Header renders page number if provided', () => {
-  // Arrange
-  const { getByText } = render(
-    <Header
-      logo="fake/image/path"
-      investigationTitle={testTitle}
-      pageNumber={22}
-      totalPages={50}
-    />
-  );
-  // Assert
-  expect(getByText(`${testTitle}: Page 22 of 50`)).toBeInTheDocument();
 });
 
 test('When TOC is visible Header TOC toggle reads "Close..."', () => {
@@ -45,7 +58,7 @@ test('When TOC is visible Header TOC toggle reads "Close..."', () => {
     <Header tocVisability toggleToc={handleToggleClick} />
   );
   // Assert
-  expect(getByRole('button')).toHaveTextContent('Close Table of Contents');
+  expect(getByRole('button')).toHaveTextContent('interface::header.toc_close');
 });
 
 test('When TOC is visible Header TOC toggle reads "Open..."', () => {
@@ -53,7 +66,7 @@ test('When TOC is visible Header TOC toggle reads "Open..."', () => {
 
   const { getByRole } = render(<Header toggleToc={handleToggleClick} />);
   // Assert
-  expect(getByRole('button')).toHaveTextContent('Open Table of Contents');
+  expect(getByRole('button')).toHaveTextContent('interface::header.toc_open');
 });
 
 test('When Header TOC toggle is clicked toggleToc callback fires"', async () => {
