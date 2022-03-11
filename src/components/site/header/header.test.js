6ea0c028b065a440ae0429d7864980bdb3f5ed0b
Alexandra Goff <agoff@lsst.org>
2022-03-11 13:02:58 -0700

    [C] remove i18n mock implementation

--- src/components/site/header/header.test.js
+++ src/components/site/header/header.test.js
@@ -1,35 +1,9 @@
 import React from 'react';
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 import { render, fireEvent } from '@testing-library/react';
 import Header from './index.jsx';
 
 const testTitle = 'Test Title';
 const handleToggleClick = jest.fn();
-
-i18n.use(initReactI18next).init({
-  lng: 'en',
-  fallbackLng: 'en',
-
-  // have a common namespace used around the full app
-  ns: ['interface'],
-  defaultNS: 'inface',
-
-  debug: true,
-
-  interpolation: {
-    escapeValue: false, // not needed for react!!
-  },
-
-  resources: {
-    en: {
-      interface: {
-        header: { toc_close: 'Close Table of Contents' },
-        locations: { table_of_contents: 'Table of Contents' },
-      },
-    },
-  },
-});
 
 test('Header renders without props', () => {
   // Arrange
