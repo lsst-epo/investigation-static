4cce020a0dd7499d90cf1ef0f70b3d6de010bac6
Alexandra Goff <agoff@lsst.org>
2022-04-29 16:06:10 -0700

    [C] add fallback for server side rendering

--- src/components/site/debug/index.jsx
+++ src/components/site/debug/index.jsx
@@ -5,29 +5,35 @@ import ls from 'local-storage';
 
 class Debug extends React.PureComponent {
   getDownloadLink = () => {
-    const savedState = JSON.stringify(ls(this.global.investigation));
-    const savedStateHash = this.hashKeys(Object.keys(savedState));
-    const state = JSON.stringify(this.global);
-    const stateHash = this.hashKeys(Object.keys(state));
-    const browser = navigator.userAgent;
-    const location = window.location.href;
-    const online = navigator.onLine;
-    const date = new Date();
-    const dimensions = `${window.innerWidth}px x ${window.innerHeight}px`;
+    if (this.isBrowser()) {
+      const savedState = JSON.stringify(ls(this.global.investigation));
+      const savedStateHash = this.hashKeys(Object.keys(savedState));
+      const state = JSON.stringify(this.global);
+      const stateHash = this.hashKeys(Object.keys(state));
+      const browser = navigator.userAgent;
+      const location = window.location.href;
+      const online = navigator.onLine;
+      const date = new Date();
+      const dimensions = `${window.innerWidth}px x ${window.innerHeight}px`;
 
-    const data = new Blob(
-      [
-        `Location: ${location}\nDate: ${date}\nBrowser: ${browser}\nOnline: ${online}\nDimensions: ${dimensions}\n\nLocal storage - ${savedStateHash}\n${savedState}\n\nState - ${stateHash}\n${state}`,
-      ],
-      { type: 'text/plain' }
-    );
-    return window.URL.createObjectURL(data);
+      const data = new Blob(
+        [
+          `Location: ${location}\nDate: ${date}\nBrowser: ${browser}\nOnline: ${online}\nDimensions: ${dimensions}\n\nLocal storage - ${savedStateHash}\n${savedState}\n\nState - ${stateHash}\n${state}`,
+        ],
+        { type: 'text/plain' }
+      );
+      return window.URL.createObjectURL(data);
+    }
+
+    return '';
   };
 
   getFileName = () => `debug-${new Date().getTime()}.txt`;
 
   hashKeys = keys =>
     keys.reduce((hash, char) => 0 | (31 * hash + char.charCodeAt(0)), 0);
+
+  isBrowser = () => typeof window !== 'undefined';
 
   render = () => (
     <a
