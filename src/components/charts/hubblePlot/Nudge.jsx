6e5d26077dc75005ffbd7264a70c428c5d3de82e
Alexandra Goff <agoff@lsst.org>
2022-10-20 13:56:41 -0700

    [C] restyle nudger, fix nudging bugs

--- src/components/charts/hubblePlot/Nudge.jsx
+++ src/components/charts/hubblePlot/Nudge.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Button from '../../site/button';
@@ -12,100 +12,133 @@ import {
   nudgeDown,
   nudgeUp,
   nudge,
+  nudgeFiller,
 } from './hubble-plot.module.scss';
 
 const Nudge = ({
   show,
-  mouseDownCallback,
-  mouseUpCallback,
+  arrowDownCallback,
+  arrowUpCallback,
   xValueAccessor,
   yValueAccessor,
   xScale,
   yScale,
   data,
   offsetTop,
-  chartScale,
 }) => {
+  const width = 80;
+  const arrows = [
+    { icon: ArrowLeft, key: 'ArrowUp', cssClass: nudgeUp, srText: 'Up' },
+    {
+      icon: ArrowRight,
+      key: 'ArrowRight',
+      cssClass: nudgeRight,
+      srText: 'Right',
+    },
+    {
+      icon: ArrowRight,
+      key: 'ArrowDown',
+      cssClass: nudgeDown,
+      srText: 'Down',
+    },
+    {
+      icon: ArrowLeft,
+      key: 'ArrowLeft',
+      cssClass: nudgeLeft,
+      srText: 'Left',
+    },
+  ];
+
   const [timer, setTimer] = useState();
   const [pressing, setPressing] = useState(false);
   const [direction, setDirection] = useState();
+  const arrowsRef = useRef();
+
   const [datum] = data || [];
   const { [xValueAccessor]: datumX, [yValueAccessor]: datumY } = datum || {};
-
   const opacity = show ? 1 : 0;
-  const left = `${xScale(datumX) * chartScale}px`;
-  const top = `${(yScale(datumY) + offsetTop) * chartScale}px`;
+  const x = xScale(datumX) - width / 2 || 0;
+  const y = yScale(datumY) + offsetTop - width / 2 || 0;
 
   useEffect(() => {
     if (pressing && direction) {
-      mouseDownCallback(direction);
-      const interval = setInterval(() => mouseDownCallback(direction), 100);
+      arrowDownCallback(direction);
+      const interval = setInterval(() => arrowDownCallback(direction), 100);
       setTimer(interval);
     } else {
       clearInterval(timer);
-      mouseUpCallback();
+      arrowUpCallback();
     }
   }, [pressing]);
 
-  const handlePressingDown = clickDirection => {
-    setPressing(true);
-    setDirection(clickDirection);
+  const handlePressingDown = (event, clickDirection) => {
+    const { button } = event;
+
+    if (button === 0) {
+      setPressing(true);
+      setDirection(clickDirection);
+    }
   };
 
   const handleNotPressingDown = () => {
     setPressing(false);
   };
 
-  return (
-    <div className={nudgeContainer} style={{ opacity, left, top }}>
-      <Button
-        icon
-        iconEl={<ButtonIcon srText="Up" Icon={ArrowLeft} />}
-        className={classNames(nudge, nudgeUp)}
-        onMouseDown={() => handlePressingDown('ArrowUp')}
-        onMouseUp={handleNotPressingDown}
-        onMouseLeave={handleNotPressingDown}
-        disabled={!show}
-      />
-      <Button
-        icon
-        iconEl={<ButtonIcon srText="Right" Icon={ArrowRight} />}
-        className={classNames(nudge, nudgeRight)}
-        onMouseDown={() => handlePressingDown('ArrowRight')}
-        onMouseUp={handleNotPressingDown}
-        onMouseLeave={handleNotPressingDown}
-        disabled={!show}
-      />
-      <Button
-        icon
-        iconEl={<ButtonIcon srText="Down" Icon={ArrowRight} />}
-        className={classNames(nudge, nudgeDown)}
-        onMouseDown={() => handlePressingDown('ArrowDown')}
-        onMouseUp={handleNotPressingDown}
-        onMouseLeave={handleNotPressingDown}
-        disabled={!show}
-      />
-      <Button
-        icon
-        iconEl={<ButtonIcon srText="Left" Icon={ArrowLeft} />}
-        className={classNames(nudge, nudgeLeft)}
-        onMouseDown={() => handlePressingDown('ArrowLeft')}
-        onMouseUp={handleNotPressingDown}
-        onMouseLeave={handleNotPressingDown}
-        disabled={!show}
-      />
-    </div>
-  );
-};
+  const onKeyDown = event => {
+    const { key } = event;
 
-Nudge.defaultProps = {
-  chartScale: 1,
+    if (datum && key.includes('Arrow')) {
+      arrowDownCallback(key);
+    }
+  };
+
+  const setEventHandlers = () => {
+    const { current: element } = arrowsRef;
+    element.addEventListener('keydown', onKeyDown);
+    element.addEventListener('keyup', arrowUpCallback);
+  };
+
+  const removeEventHandlers = () => {
+    const { current: element } = arrowsRef;
+    element.removeEventListener('keydown', onKeyDown);
+    element.removeEventListener('keyup', arrowUpCallback);
+  };
+
+  return (
+    <foreignObject {...{ x, y, width }} height={width}>
+      <div
+        className={nudgeContainer}
+        style={{ opacity }}
+        onFocus={setEventHandlers}
+        onBlur={removeEventHandlers}
+        ref={arrowsRef}
+      >
+        {arrows.map(arrow => {
+          const { icon: Icon, key, cssClass, srText } = arrow;
+
+          return (
+            <Button
+              key={key}
+              icon
+              iconEl={<ButtonIcon {...{ Icon, srText }} />}
+              className={classNames(nudge, cssClass)}
+              onMouseDown={e => handlePressingDown(e, key)}
+              onMouseUp={handleNotPressingDown}
+              onMouseLeave={handleNotPressingDown}
+              disabled={!show}
+            />
+          );
+        })}
+        <div className={nudgeFiller} />
+      </div>
+    </foreignObject>
+  );
 };
 
 Nudge.propTypes = {
   show: PropTypes.bool,
-  mouseDownCallback: PropTypes.func,
-  mouseUpCallback: PropTypes.func,
+  arrowDownCallback: PropTypes.func,
+  arrowUpCallback: PropTypes.func,
   offsetTop: PropTypes.number,
   xValueAccessor: PropTypes.string,
   yValueAccessor: PropTypes.string,
