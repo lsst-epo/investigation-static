2f6fc4f3099a0378e6f053f5d14ed673757ad4fc
Alexandra Goff <agoff@lsst.org>
2022-10-27 15:11:03 -0700

    [C] update to styling and sectionbreaks
    For JIRA: "EPO-7155 #IN-REVIEW #comment update to styling and sectionbreaks"

--- src/components/page/sectionBreak.jsx
+++ src/components/page/sectionBreak.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'reactn';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'gatsby-plugin-react-i18next';
 import ImageLoader from '../site/imageLoader';
@@ -7,7 +7,14 @@ import Block from './blocks/index';
 class SectionBreak extends React.PureComponent {
   render() {
     const { blocksGroups, shared: blockShared, t } = this.props;
-    const sectionBreakImagePath = '/images/section_break_celebration.gif';
+    const { section } = blockShared;
+    const { sections } = this.global;
+    const isFinalBreak = section === sections.length - 2;
+    const sectionBreakImgRoot = '/images/section_break/';
+    const sectionBreakFileName = isFinalBreak
+      ? t('interface::section_break.final_image')
+      : 'section_break_celebration.gif';
+    const sectionBreakImagePath = `${sectionBreakImgRoot}${sectionBreakFileName}`;
 
     return (
       <div className="section-break">
