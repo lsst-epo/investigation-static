ac95b9afcd582b361a872a698d019d559a6fa304
Alexandra Goff <agoff@lsst.org>
2022-12-28 10:59:47 -0700

    [C] add Spanish checkpoint SVG

--- src/components/site/checkpoint/index.jsx
+++ src/components/site/checkpoint/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useTranslation } from 'gatsby-plugin-react-i18next';
-import svg from './checkpoint.svg';
+import en from './svg/checkpoint-en.svg';
+import es from './svg/checkpoint-es.svg';
 import { checkpointContainer } from './checkpoint.module.scss';
 
 const Checkpoint = () => {
-  const { t } = useTranslation('interface');
+  const svg = { en, es };
+  const { t, i18n } = useTranslation('interface');
+  const { language } = i18n;
+
   return (
     <div className={checkpointContainer}>
-      <img
-        className="checkpoint-svg"
-        src={svg}
-        alt={t('locations.checkpoint')}
-      />
+      <img src={svg[language] || en} alt={t('locations.checkpoint')} />
     </div>
   );
 };
